0b75f9115eb27dc64d1e7537f09b23d9b0ade6b1
Hugo Pigree <116163096+HugoPigree@users.noreply.github.com>
2024-11-03 18:09:27 +0100

    css

--- src/Accueil.jsx
+++ src/Accueil.jsx
@@ -7,8 +7,7 @@ import Citation from "./Citation/Citation";
 import Blague from "./Blague/Blague";
 import React from "react";
 import Header from "./Header";
-import "../public/assets/styles.css"
-
+import "../public/assets/styles.css";
 
 const Accueil = () => {
   const navigate = useNavigate();
@@ -20,47 +19,32 @@ const Accueil = () => {
   return (
     <div>
       <div className="Container">
-      <Header />
+        <Header />
         <div className="Top">
           <Time></Time>
         </div>
 
         <div className="main">
-
-        <div className="left">
-          <Blague></Blague>
-          <Citation></Citation>
-          <div className="Historique">
-          <Recette></Recette>
+          <div className="left">
+            <Blague></Blague>
+            <Citation></Citation>
+            <div className="Historique">
+              <Recette></Recette>
+            </div>
           </div>
-        </div>
 
-        <div className="center">
-          <h1>Hey</h1>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga
-            perspiciatis, eos soluta quod reiciendis quisquam, repudiandae
-            quibusdam dicta doloribus facilis illum voluptates, labore nulla
-            beatae voluptatum qui deserunt vero blanditiis! Maxime corrupti ad
-            quasi id reprehenderit ab voluptatem enim omnis molestiae quam
-            mollitia autem impedit odit, nobis beatae expedita odio dolorum modi
-            quidem, at eligendi eveniet! Nisi vitae tenetur architecto.
-            Molestiae quos maxime, atque praesentium, asperiores quas est a enim
-            vel repellat, sint dolores nulla accusantium suscipit. Cumque libero
-            sapiente mollitia atque nesciunt iure, nobis aliquid minima impedit
-            quas similique!
-          </p>
-          <div>
-          <button className="markdown-button" onClick={Markdownzone}>
-            Creer un fichier Markdown
-          </button>
-        </div>
-        </div>
+          <div className="center">
+            <div>
+              <button className="markdown-button" onClick={Markdownzone}>
+                Creer un fichier Markdown
+              </button>
+            </div>
+          </div>
 
-        <div className="right">
-        <MocktailOfTheDay></MocktailOfTheDay>
-        <EvenementsHistoriques></EvenementsHistoriques>
-        </div>
+          <div className="right">
+            <MocktailOfTheDay></MocktailOfTheDay>
+            <EvenementsHistoriques></EvenementsHistoriques>
+          </div>
         </div>
       </div>
     </div>
